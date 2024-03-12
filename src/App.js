434c926d79a5cde578cee81f7964359f59a05b19
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-12 21:37:24 +0530

    imported navbar component in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route, NavLink} from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <div className='min-h-screen p-6 bg-white text-gray-600 text-lg' >
+        <Navbar/>
 
       </div>
       
