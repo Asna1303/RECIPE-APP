6cd1dfac50d85ab5fe3a627d4ada1f75ac53e147
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-13 21:59:42 +0530

    imported dtails in app.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import {Routes,Route, NavLink} from 'react-router-dom';
 import Navbar from './components/navbar';
 import Home from './pages/home';
-import Favorites from './pages/favorites';
+import Favorites from './pages/favorites';                                                                                    
 
+import Details from './pages/details'; 
 
 
 function App() {
