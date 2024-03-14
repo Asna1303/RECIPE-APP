b84bf2862f31995fb8eb6302bf63b38a79b04a0a
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-14 09:15:35 +0530

    defined classname

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,5 +1,5 @@
 export default function Navbar(){
-    return <nav>
-        
+    return <nav className="flex justify-between item-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+
     </nav>
 }
