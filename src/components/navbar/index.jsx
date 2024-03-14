3682a3acce05f28078c91d0706927ab7b04cc257
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-14 09:24:14 +0530

    started form:gave a input

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,5 +3,13 @@ export default function Navbar(){
 <h2 className="text-2xl font-semibold">
    FoodRecipe 
 </h2>
+<form>
+    <input
+    type="text"
+    name="search"
+    placeholder="Enter Items..."
+    className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
+    />
+</form>
     </nav>
 }
